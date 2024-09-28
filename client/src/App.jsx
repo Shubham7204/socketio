5f3f7385a04866322ba6c0ca0fccacc27ab9f7cc
Shubham7204 <onlinelearner01learn@gmail.com>
2024-09-28 22:04:13 +0530

    useeffect

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
-import {io} from 'socket.io-client'
-const App = () => {
+import React, { useEffect } from 'react';
+import { io } from 'socket.io-client';
 
-  const socket = io('http://localhost:3000')
-  
+const App = () => {
+  const socket = io('http://localhost:3000');
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      console.log('Connected to server', socket.id);
+    });
+
+    socket.on('welcome', (message) => {
+      console.log(message);
+    });
+
+    return () => {
+      socket.disconnect(); // Clean up the socket connection when the component unmounts
+    };
+  }, []);
+
   return (
     <div>
-      <h1 className='text-red-600 text-4xl text-center m-4' >Learning Socket.io</h1>
+      <h1 className='text-red-600 text-4xl text-center m-4'>Learning Socket.io</h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- server/index.js
+++ server/index.js
@@ -1,43 +1,40 @@
 import express from 'express';
-import { Server } from "socket.io";
-import { createServer } from "http";
-import cors from "cors";
+import { Server } from 'socket.io';
+import { createServer } from 'http';
+import cors from 'cors';
 
 const port = 3000;
 const app = express();
 const server = createServer(app);
 
 app.use(cors({
-    origin: "http://localhost:5173", 
-    methods: ["GET", "POST"],
-    credentials: true,
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST'],
+  credentials: true,
 }));
 
-// Create socket.io server
 const io = new Server(server, {
-    cors: {
-        origin: "http://localhost:5173", 
-        methods: ["GET", "POST"],
-    }
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+  }
 });
 
-// Socket.io connection event
-io.on("connection", (socket) => {
-    console.log("User connected");
-    console.log("ID", socket.id);
+io.on('connection', (socket) => {
+  console.log('User connected');
+  console.log('ID:', socket.id);
 
-    // Optionally handle disconnection
-    socket.on('disconnect', () => {
-        console.log('User disconnected');
-    });
+  socket.emit('welcome', `Welcome to the server, ${socket.id}`);
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
 });
 
-// HTTP route for testing
 app.get('/', (req, res) => {
-    res.send('Hello World');
+  res.send('Hello World');
 });
 
-// Start the server
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+  console.log(`Server is running on port ${port}`);
 });
