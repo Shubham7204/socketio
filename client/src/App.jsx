1c072b191e348a5ff284de09187a7f575ec4c580
Shubham7204 <onlinelearner01learn@gmail.com>
2024-09-28 21:50:12 +0530

    user connected

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
-
+import {io} from 'socket.io-client'
 const App = () => {
+
+  const socket = io('http://localhost:3000')
+  
   return (
     <div>
       <h1 className='text-red-600 text-4xl text-center m-4' >Learning Socket.io</h1>
--- server/index.js
+++ server/index.js
@@ -1,22 +1,43 @@
 import express from 'express';
 import { Server } from "socket.io";
-import {createServer} from "http";
+import { createServer } from "http";
+import cors from "cors";
 
 const port = 3000;
 const app = express();
 const server = createServer(app);
 
-const io = new Server(server);
+app.use(cors({
+    origin: "http://localhost:5173", 
+    methods: ["GET", "POST"],
+    credentials: true,
+}));
 
-io.on("Connection", (socket)=>{
+// Create socket.io server
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:5173", 
+        methods: ["GET", "POST"],
+    }
+});
+
+// Socket.io connection event
+io.on("connection", (socket) => {
     console.log("User connected");
     console.log("ID", socket.id);
-})
 
+    // Optionally handle disconnection
+    socket.on('disconnect', () => {
+        console.log('User disconnected');
+    });
+});
+
+// HTTP route for testing
 app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(port, () => {
+// Start the server
+server.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
