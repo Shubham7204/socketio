5445d8b63cd6ba19da6e3c53b935a1f4c27225f4
Shubham7204 <onlinelearner01learn@gmail.com>
2024-09-28 21:05:03 +0530

    const server = createServer(app);

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 import express from 'express';
+import { Server } from "socket.io";
+import {createServer} from "http";
 
 const port = 3000;
 const app = express();
+const server = createServer(app);
+
+const io = new Server(server);
+
+io.on("Connection", (socket)=>{
+    console.log("User connected");
+    console.log("ID", socket.id);
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World');
